fe2139f5a997fb4a1e05177f1b6be53fa8d58c51
ankurp <ankur.patel@ymail.com>
2021-05-21 10:00:28 -0400

    1 file modified

--- index.js
+++ index.js
@@ -13,43 +13,47 @@ const {
 } = sdk;
 
 // function to encode file data to base64 encoded string
-async function base64Encode(url) {
-  const bitmap = await fetch(url);
+function base64Encode(filePath) {
+  const bitmap = fs.readFileSync(filePath);
   return new Buffer.from(bitmap).toString('base64');
 }
 
-const data = base64Encode('');
+const data = base64Encode(
+  './GCEE5H3RI2MFP4UQ4NHFKLGTIHILWA775AM7KTLU5HUBSLOBJN7M4RSL.png'
+);
 
-// const server = new Server('https://horizon-testnet.stellar.org');
-// const sourceKey = Keypair.fromSecret(
-//   'SBELXHH4YQRLYPCETDMRGTHJGOGTJHDZRCLJM34OT3J23DERVQW542FZ'
-// );
+const server = new Server('https://horizon-testnet.stellar.org');
+const sourceKey = Keypair.random();
 
-// await fetch(`https://friendbot.stellar.org?addr=${sourceKey.publicKey()}`);
+console.log('Public Key: ', sourceKey.publicKey());
 
-// const sourceAccount = await server.loadAccount(sourceKey.publicKey());
-// const transactionBuilder = new TransactionBuilder(sourceAccount, {
-//   fee: BASE_FEE * 100,
-//   networkPassphrase: Networks.TESTNET
-// });
+(async () => {
+  await fetch(`https://friendbot.stellar.org?addr=${sourceKey.publicKey()}`);
 
-// const chunksOfData = data.split('');
+  const sourceAccount = await server.loadAccount(sourceKey.publicKey());
+  const transactionBuilder = new TransactionBuilder(sourceAccount, {
+    fee: BASE_FEE * 100,
+    networkPassphrase: Networks.TESTNET
+  });
 
-// for (let j = 0; chunksOfData.length > 0; j++) {
-//   transactionBuilder.addOperation(
-//     Operation.manageData({
-//       name: ('00' + j).slice(-2) + chunksOfData.splice(0, 62).join(''),
-//       value: chunksOfData.splice(0, 64).join('')
-//     })
-//   );
-// }
+  const chunksOfData = data.split('');
 
-// const transaction = transactionBuilder.setTimeout(TimeoutInfinite).build();
-// transaction.sign(sourceKey);
+  for (let j = 0; chunksOfData.length > 0; j++) {
+    transactionBuilder.addOperation(
+      Operation.manageData({
+        name: ('00' + j).slice(-2) + chunksOfData.splice(0, 62).join(''),
+        value: chunksOfData.splice(0, 64).join('')
+      })
+    );
+  }
 
-// try {
-//   const result = await server.submitTransaction(transaction);
-//   console.log('Success! Results:', result);
-// } catch (e) {
-//   console.error('ERROR: ', e);
-// }
+  const transaction = transactionBuilder.setTimeout(TimeoutInfinite).build();
+  transaction.sign(sourceKey);
+
+  try {
+    const result = await server.submitTransaction(transaction);
+    console.log('Success! Results:', result);
+  } catch (e) {
+    console.error('ERROR: ', e);
+  }
+})();
